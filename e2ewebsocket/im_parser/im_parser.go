744628f44d26434ff0edb3a958bd3641fecc3c9c
qs3c <2749950753@qq.com>
2026-03-24 18:33:43 +0800

    补充了点注释，准备重新开工了！

--- e2ewebsocket/im_parser/im_parser.go
+++ e2ewebsocket/im_parser/im_parser.go
@@ -12,14 +12,14 @@ type IMParser interface {
 	ConstructMsgData(sendID, recvID string, msg []byte) MsgData
 
 	// 写方向 序列化与反序列化方法 
-	// MsgData -> []byte -> Server
-	// MsgData <- []byte <- Server
+	// MsgData -> []byte -> Server 【之前client已有】
+	// MsgData <- []byte <- Server	【本次需补充的方向，与服务器收到客户端消息后的解析逻辑相同】
 	MsgDataToServerWriteBound(msgData MsgData) ([]byte, error)
 	MsgDataFromServerWriteBound(data []byte) (MsgData, error)
 
 	// 读方向 序列化与反序列化方法
-	// Server -> []byte -> MsgData
-	// Server <- []byte <- MsgData
+	// Server -> []byte -> MsgData	【之前client已有】
+	// Server <- []byte <- MsgData	【本次需补充的方向，与服务器组织消息发送给客户端的逻辑相同】
 	MsgDataToServerReadBound(msgData MsgData) ([]byte, error)
 	MsgDataFromServerReadBound(data []byte) (MsgData, error)
 }
